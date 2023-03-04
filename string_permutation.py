ac783457178dd04deb6eb24ebdaf601ed62d63f2
Shiminc <shiminc@gmail.com>
2023-03-04 12:49:33 +0000

    three letters

--- string_permutation.py
+++ string_permutation.py
@@ -9,13 +9,20 @@ def split_word(word):
 def get_permutation(word=list):
     if len(word)==1:
         return word
-    else:
+    elif len(word)==2:
         return [word[0]+word[1],word[1]+word[0]]
+    elif len(word)==3:
+        return [word[0]+word[1]+word[2],
+                word[0]+word[2]+word[1],
+                word[1]+word[0]+word[2],
+                word[1]+word[2]+word[0],
+                word[2]+word[0]+word[1],
+                word[2]+word[1]+word[0]]
     
 
 def main():
-    word=split_word("ab")
-    print(get_permutation(word))
+    word=split_word("aac")
+    print(set(get_permutation(word)))
 
 if __name__ == "__main__":
     main()
